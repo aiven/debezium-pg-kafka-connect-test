cba61665cbeac802ac5eac5f3f48dd93fd81b1da
Viet Nguyen <vietn@aiven.io>
2021-06-25 11:21:48 +1000

    some changes to replication_slot_monitor.py
    - Add parameter to specify debezium replication slot name
    - Ran automatic reformatting tools isort and yapf

--- bin/python_scripts/replication_slot_monitor.py
+++ bin/python_scripts/replication_slot_monitor.py
@@ -5,36 +5,37 @@ with debezium_pg_monitor.py and then simulate service genbump using `avn-test se
 recycle <service-id> --no-reason` in order to observe what happens during a genbump
 and see if the Debezium connector properly recovers or not.
 """
-from utils import get_pg_and_kafka_connection_info
-
 import argparse
-import psycopg2
+import datetime
 import logging
 import time
-import datetime
 import traceback
+
+import psycopg2
+from utils import get_pg_and_kafka_connection_info
 
 logging.basicConfig(
     level=logging.ERROR,
     format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s: %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
-)
+    datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger("replication_slot_monitor")
 
-Ki: int = 2 ** 10
-Mi: int = 2 ** 20
-Gi: int = 2 ** 30
+Ki: int = 2**10
+Mi: int = 2**20
+Gi: int = 2**30
 
-WARN_SLOT_INACTIVE_TIMEOUT = 10 # warning when replication slot has been inactive
+WARN_SLOT_INACTIVE_TIMEOUT = 10  # warning when replication slot has been inactive
 active_checkpoint = datetime.datetime.now()
 
-def periodic_replication_slot_check(cursor, sleep=5.0):
+
+def periodic_replication_slot_check(cursor,
+                                    debezium_slot_name="debezium",
+                                    sleep=5.0):
     global inserted_records, total_inserted_records, active_checkpoint
 
     while True:
         try:
-            cursor.execute(
-                """
+            cursor.execute("""
                 SELECT
                     slot_name,
                     active,
@@ -42,16 +43,19 @@ def periodic_replication_slot_check(cursor, sleep=5.0):
                     sum(pg_catalog.pg_wal_lsn_diff(pg_catalog.pg_current_wal_lsn(), restart_lsn)::BIGINT)::BIGINT AS bytes_diff
                 FROM pg_catalog.pg_replication_slots
                 GROUP BY slot_name, active, restart_lsn
-                """
-            )
-
+                """)
+            checked = False
             for record in cursor:
-                if record[0] == "debezium":
+                if record[0] == debezium_slot_name:
                     # check for slot active state
-                    active_time_elapsed = round((datetime.datetime.now() - active_checkpoint).total_seconds())
+                    active_time_elapsed = round(
+                        (datetime.datetime.now() -
+                         active_checkpoint).total_seconds())
                     active = record[1]
                     if not active and active_time_elapsed > WARN_SLOT_INACTIVE_TIMEOUT:
-                        logger.warning(f"Replication slot is not active for {active_time_elapsed} seconds!")
+                        logger.warning(
+                            f"Replication slot is not active for {active_time_elapsed} seconds!"
+                        )
 
                     if active:
                         active_checkpoint = datetime.datetime.now()
@@ -62,14 +66,25 @@ def periodic_replication_slot_check(cursor, sleep=5.0):
                     if bytes_diff < 1000:
                         logger.info(f"Debezium slot lag: {bytes_diff} bytes")
                     elif Mi >= bytes_diff >= Ki:
-                        logger.info(f"Debezium slot lag: {bytes_diff // Ki} KiB")
+                        logger.info(
+                            f"Debezium slot lag: {bytes_diff // Ki} KiB")
                     elif bytes_diff > Mi:
-                        logger.info(f"Debezium slot lag: {bytes_diff // Mi} MiB")
+                        logger.info(
+                            f"Debezium slot lag: {bytes_diff // Mi} MiB")
+                    checked = True
+            if not checked:
+                logger.warning(
+                    f"Debezium replication slot '{debezium_slot_name}' does not exist!"
+                )
+            else:
+                checked = False
 
             time.sleep(sleep)
 
         except (psycopg2.errors.AdminShutdown, psycopg2.InterfaceError):
-            logger.error("Failed connecting to the PG server. Retrying after 5 seconds...")
+            logger.error(
+                "Failed connecting to the PG server. Retrying after 5 seconds..."
+            )
             _, pg_uri, _ = get_pg_and_kafka_connection_info()
 
             try:
@@ -87,25 +102,26 @@ def periodic_replication_slot_check(cursor, sleep=5.0):
             logger.error("Failed. Retrying...")
 
 
-
 parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--verbose",
-    action="store_true",
-    help="Shows all log levels. Default log level is WARN"
-)
-parser.add_argument(
-    "--sleep",
-    type=float,
-    default=5.0,
-    help="Delay between checks. Default 5 second")
+parser.add_argument("--verbose",
+                    action="store_true",
+                    help="Shows all log levels. Default log level is WARN")
+parser.add_argument("--sleep",
+                    type=float,
+                    default=5.0,
+                    help="Delay between checks. Default 5 second")
+parser.add_argument("--debezium-slot-name",
+                    type=str,
+                    default="debezium",
+                    help="Debezium slot name. Default 'debezium'")
 
 if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.verbose:
         logger.setLevel(logging.DEBUG)
-    else: logger.setLevel(logging.WARN)
+    else:
+        logger.setLevel(logging.WARN)
 
     _, pg_uri, config = get_pg_and_kafka_connection_info()
 
@@ -114,9 +130,9 @@ if __name__ == "__main__":
     logger.info("Connected!\n")
 
     try:
-        periodic_replication_slot_check(cursor, args.sleep)
+        periodic_replication_slot_check(cursor, args.debezium_slot_name,
+                                        args.sleep)
     except KeyboardInterrupt:
         conn.close()
         cursor.close()
         logger.info("Closed connections.")
-
